e2aab0f3e129ed3ec014790489a8330791de7b61
FXtreme <m10226677@gmail.com>
2024-07-10 17:50:54 +0700

    add index

--- thai_school_alarm_web/urls.py
+++ thai_school_alarm_web/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from data import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("/"),
+    path("",views.index),
     # other Django apps
     path("__reload__/", include("django_browser_reload.urls")),
 ]
